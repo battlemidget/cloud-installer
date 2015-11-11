a11e6470f5bddb73aaa02bd15ada8f6c60732dca
Adam Stokes <adam.stokes@ubuntu.com>
2015-11-11 09:32:12 -0500

    wut
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ cloudinstall/ui/views/step.py
@@ -0,0 +1,54 @@
+# Copyright 2014, 2015 Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from urwid import (WidgetWrap, Button, Pile,
+                   Divider, Text, Filler)
+from cloudinstall.ui.utils import Padding, Color
+
+
+def show_step_info(frame, msg):
+    """ Shows a information panel view
+
+    Arguments:
+    frame: urwid.Frame
+    msg: message to be displayed
+    """
+    frame.body = StepInfoView(msg)
+
+
+class StepInfoView(WidgetWrap):
+
+    def __init__(self, msg=None):
+        if not msg:
+            msg = "Processing."
+        items = [
+            Padding.center_60(Text("Information", align="center")),
+            Padding.center_60(
+                Divider("\N{BOX DRAWINGS LIGHT HORIZONTAL}", 1, 1)),
+            Padding.center_60(Text(msg))
+        ]
+        super().__init__(Filler(Pile(items), valign='middle'))
+
+    def _build_buttons(self):
+        buttons = [
+            Padding.line_break(""),
+            Color.button_secondary(
+                Button("Quit", self.cancel),
+                focus_map='button_secondary focus'),
+        ]
+        return Pile(buttons)
+
+    def cancel(self, button):
+        raise SystemExit("Installation cancelled.")
